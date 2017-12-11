a685f080f52f4c2623f17f0a12ff7485b1ca85b5
github username <github email address>
2017-12-10 20:46:07 -0500

    Revert "trying to authenticate with back end"
    This reverts commit 083725a86cbe0170fb68bbd5e95e75a536c40473.

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,7 +1,5 @@
 export const AUTH_CONFIG = {
-  domain: 'nvcapiside.auth0.com',
+  domain: 'backapi.auth0.com',
   clientId: 'EYhYVaVufT1OHRpdLnqYIUdLcJQu9yBM',
   callbackUrl: 'http://localhost:3001/callback'
 }
-
-
